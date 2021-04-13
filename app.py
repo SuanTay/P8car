7ca7ec848256686fe68571e617029f162285a536
Suan <suan.tay@gmail.com>
2021-04-13 13:53:07 +0200

    static path

--- app.py
+++ app.py
@@ -1,41 +1,47 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm, validators
-from wtforms import  IntegerField
+from requests.models import Response
+from wtforms import IntegerField
 from PIL import Image
 import os
 import numpy as np
 from wtforms.validators import NumberRange
 import requests
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 class numForm(FlaskForm):
     num_img = IntegerField("Numéro de l'image entre 0 et 9 :", validators=[NumberRange(min=0, max=9)])
 
+
 def comput(i):
+    static_path = os.path.join(APP_ROOT, 'static/')
+    print('static_url_path',static_path)
     # recuperation de l'image suivant un index depuis le Blob d'Azure
     image_dir = os.getenv('path_img')
-    print('image dir ',image_dir)
-    image_mask = ""
+    print('image dir ', image_dir)
     image_list = os.listdir(image_dir)
     i = int(i)
     path = f'{image_dir}/{image_list[i]}'
-    print('Image path ',path)
+    print('Image path ', path)
     name = f'{image_list[i]}'
-    print('name img ',name)
-    #copie l'image dans le dossier static
+    print('name img ', name)
+    # copie l'image dans le dossier static
     imgR = Image.open(path)
-    imgR.save(name)
+    print('Path enr', f'{static_path}{name}')
+    imgR.save(f'{static_path}{name}')
+    print('Saved img Original')
     # image segmentation map
     imgC = myapi(imgR)
-    name_c=f'c_{name}'
+    name_c = f'c_{name}'
     print('name color ', name_c)
-    imgC.save(name_c)
-    #merge
-    imgT = merge(imgC, imgR)
-    name_t=f't_{name}'
+    imgC.save(static_path + name_c)
+    # merge
+    imgT = merge(static_path + imgC, imgR)
+    name_t = f't_{name}'
     print('name total ', name_t)
     imgT.save(name_t)
     print('Image path original', name)
@@ -44,24 +50,31 @@ def comput(i):
 
     return name, name_c, name_t
 
+
 def merge(imgPred, img):
     imgPred = imgPred.resize((512, 256))
     img = img.resize((512, 256))
     imgs = Image.blend(img, imgPred, 0.6)
     return imgs
 
+
 def myapi(data):
     uri = os.getenv('endpoint')
-    print('get endpoint OK')
+    print('myapi:1 ,get endpoint OK')
     key = os.getenv('key')
-    print('get key OK')
+    print('myapi:2 ,get key OK')
     input_data = serialize_image(data)
+    print('myapi:3 ,input_data OK')
     headers = {'Content-Type': 'application/json'}
     headers['Authorization'] = f'Bearer {key}'
 
-    response = requests.post(uri,data = input_data, headers=headers)
-    print('response OK')
-    return deserialize_image(response)
+    response: Response = requests.post(uri, data=input_data, headers=headers)
+    print('myapi:4 :response OK')
+    print('myapi:5', response.json)
+    json_file = deserialize_image(response.json)
+    print('myapi:6', json_file[:50])
+    return json_file
+
 
 def serialize_image(image):
     import json
@@ -71,8 +84,8 @@ def serialize_image(image):
     memfile = io.BytesIO()
     np.save(memfile, img)
     memfile.seek(0)
-    data_serialized = json.dumps({'data':memfile.read().decode('latin-1')})
-    return(data_serialized)
+    data_serialized = json.dumps({'data': memfile.read().decode('latin-1')})
+    return (data_serialized)
 
 
 def deserialize_image(data_serialized):
@@ -86,7 +99,7 @@ def deserialize_image(data_serialized):
     print('deserialize_image shape', img.shape)
 
     image = Image.fromarray(img)
-    return(image)
+    return (image)
 
 
 @app.route('/form', methods=['GET', 'POST'])
@@ -94,9 +107,9 @@ def form():
     form = numForm()
 
     if form.validate_on_submit():
-        path , img_col_path, img_t_path = comput(form.num_img.data)
+        path, img_col_path, img_t_path = comput(form.num_img.data)
 
-        return render_template('img.html', img_path=path, img_col = img_col_path, img_t = img_t_path )
+        return render_template('img.html', img_path=path, img_col=img_col_path, img_t=img_t_path)
 
     return render_template('form.html', form=form)
 
